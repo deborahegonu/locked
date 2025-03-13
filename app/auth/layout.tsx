fe4f32b7c617d3ff17dc900e2f20f6c654766e62
deborahegonu <135129672+deborahegonu@users.noreply.github.com>
2025-03-13 11:23:40 +0100

    initialise prima ORM

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -7,7 +7,7 @@ interface AuthLayoutProps {
 export default function AuthLayout({ children } : AuthLayoutProps) {
     return(
         <div className="min-h-[85vh] flex items-center justify-center">
-            <Card className="min-w-sm border-0 shadow-none rounded-none">
+            <Card className="w-sm border-0 shadow-none rounded-none">
                 {children}
             </Card>
             
